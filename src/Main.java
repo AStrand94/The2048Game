9f38a20027615e15698f8a168edf2faa74bf2a44
Andreas Strand <strand118@hotmail.com>
2018-02-04 20:55:39 +0100

    Backend should be working as expected

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
-            primaryStage = primaryStage;
             Parent root = FXMLLoader.load(getClass().getResource("./2048Game.fxml"));
 
             Scene scene = new Scene(root);
